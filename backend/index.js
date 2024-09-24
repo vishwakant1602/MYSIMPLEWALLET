66377fa91bbf30e9823a2fa0ef90b747bfd91e7d
vishwakant1602 <121593799+vishwakant1602@users.noreply.github.com>
2024-09-24 23:50:52 +0530

    create user mongoose schema, routing file structure, route user request

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const mainrouter = require("./routes/index");
+const app = express()
 
 
+app.use("/api/v1", mainrouter)
+// app.use("/api/v2", v2Router)
+
+
+ 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,12 @@
 // backend/api/index.js
 const express = require('express');
+const userRouter = require("./user");
 
 const router = express.Router();
+
+// router.get("/user", (req,res)=> {
+
+// })
+router.use("/user", userRouter);
 
 module.exports = router;
