5d13c5d809030aa18c189674646d547fb4a8ccc0
vishwakant1602 <121593799+vishwakant1602@users.noreply.github.com>
2024-09-25 00:36:55 +0530

    added backend auth routes - Signup,Sigin ans set all Middleware

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const mainrouter = require("./routes/index");
+
+//added cors
+const cors = require("cors");
+app.use(cors());
+
+//added body parser
+const rootRouter = require("./routes/index");
+
 const app = express()
 
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1", mainrouter)
 // app.use("/api/v2", v2Router)
 
+// module.exports = router;
+
+app.listen(3000);
 
  
